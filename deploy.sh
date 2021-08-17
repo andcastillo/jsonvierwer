bb77e64eff363f70e57171ce3947fafb87513e9f
andres castillo <andcastillo@gmail.com>
2021-08-17 10:02:25 -0500

    add configuration and deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm Dockerfile
+wget https://raw.githubusercontent.com/andcastillo/jsonvierwer/main/Dockerfile
+docker stop jsonviewer
+docker build -t jsonviewer .
+docker run -d --rm -p 80:3000 --name jsonviewer jsonviewer
