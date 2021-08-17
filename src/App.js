ba2053dcd01cc93ce5f393b7ce36e15a5f4290ca
andres castillo <andcastillo@gmail.com>
2021-08-17 08:00:08 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { Container } from "reactstrap"
+import React, {useEffect, useState, useMemo } from 'react';
+import TableC from "./TableC"
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const response = await fetch('https://api.publicapis.org/entries');
+      const responseJson = await response.json();
+
+      let cols = [];
+      let entry = responseJson.entries[0];
+      for (let key of Object.keys(entry)) {
+        cols.push({'Header': key, 'accessor': key})
+      }
+      responseJson['columns'] = cols;
+      setData(responseJson.entries);
+      setColumns(responseJson.columns)
+    };
+    doFetch();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ marginTop: 100 }}>
+      <TableC columns={columns} data={data} />
+    </Container>
   );
+
 }
 
 export default App;
