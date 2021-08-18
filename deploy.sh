33a2d7a6f9a38aeb84aec5768176b001b37ea313
Andres Castillo <andcastillo@gmail.com>
2021-08-18 10:10:28 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Execute this script to deploy the jsonviewer into the AWS instance. 
+# We can move the deploy sequence into a tool for CD
 rm Dockerfile
 wget https://raw.githubusercontent.com/andcastillo/jsonvierwer/main/Dockerfile
 docker stop jsonviewer
